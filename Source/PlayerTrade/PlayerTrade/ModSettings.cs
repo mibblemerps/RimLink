533070fac8ee89ac4f5f7a04fe55737dc7a31e4c
Mibble <mibble@outlook.com.au>
2021-05-31 21:57:53 +0930

    Fixed mod settings defaults not working. Fixes #32

--- Source/PlayerTrade/PlayerTrade/ModSettings.cs
+++ Source/PlayerTrade/PlayerTrade/ModSettings.cs
@@ -5,18 +5,18 @@ namespace PlayerTrade
     public class ModSettings : Verse.ModSettings
     {
         public string ServerIp;
-        public int ServerPort;
-        public bool LoggingEnabled;
-        public bool MainMenuWidgetEnabled;
-        public bool ChatNotificationsEnabled;
+        public int ServerPort = 35562;
+        public bool LoggingEnabled = false;
+        public bool MainMenuWidgetEnabled = true;
+        public bool ChatNotificationsEnabled = true;
 
         public override void ExposeData()
         {
             Scribe_Values.Look(ref ServerIp, "server_ip");
-            Scribe_Values.Look(ref ServerPort, "server_port", 35562);
-            Scribe_Values.Look(ref LoggingEnabled, "logging_enabled", false);
-            Scribe_Values.Look(ref MainMenuWidgetEnabled, "main_menu_widget_enabled", true);
-            Scribe_Values.Look(ref ChatNotificationsEnabled, "chat_notifications", true);
+            Scribe_Values.Look(ref ServerPort, "server_port");
+            Scribe_Values.Look(ref LoggingEnabled, "logging_enabled");
+            Scribe_Values.Look(ref MainMenuWidgetEnabled, "main_menu_widget_enabled");
+            Scribe_Values.Look(ref ChatNotificationsEnabled, "chat_notifications");
             base.ExposeData();
         }
     }
