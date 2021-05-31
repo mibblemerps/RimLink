957cd3cfa8863bbef539e3aa3bd2602208746380
Mibble <mibble@outlook.com.au>
2021-06-01 06:05:01 +0930

    I am small brain and set the wrong default port for the server. 35565 -> 35562

--- Source/PlayerTrade/TradeServer/ServerSettings.cs
+++ Source/PlayerTrade/TradeServer/ServerSettings.cs
@@ -5,7 +5,7 @@ namespace TradeServer
     public class ServerSettings
     {
         public GameSettings GameSettings = new GameSettings();
-        public int Port = 35565;
+        public int Port = 35562;
         public int MaxPlayers = 64;
         public bool LogPacketTraffic = true;
     }
